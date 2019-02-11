babc6dea0e9fa5944b82e20f14fba57e467fae8e
Joseph-Kung <joseph.kung@hotmail.com>
2019-02-10 17:02:47 -0800

    Create recruiters controller

---
+++ app/controllers/recruiters_controller.rb
@@ -0,0 +1,53 @@
+class RecruitersController < ApplicationController
+  def index
+    @recruiters = Recruiter.all
+    render :index
+  end
+
+  def create
+    @recruiter = Recruiter.new(recruiter_params)
+
+    if @recruiter.save
+      render :index
+    else
+      render json: @recruiter.errors.full_messages, status: 400
+    end
+  end
+
+  def show
+    @recruiter = Recruiter.find(params[:id])
+
+    if @recruiter
+      render :show
+    else
+      render json ['Recruiter does not exit'], status: 400
+    end
+
+  end
+
+  def delete
+    @recruiter = Recruiter.find(params[:id])
+
+    if @recruiter
+      render :index
+    else
+      render json ['Recruiter does not exit'], status: 400
+    end
+  end
+
+  def update
+    @recruiter = Recruiter.find(params[id])
+
+    if @recruiter.update_attributes(recruiter_params)
+      render :show
+    else
+      render json ['Recruiter does not exit'], status: 400
+    end
+  end
+
+  private
+  
+  def recruiter_params
+    params.require(:recruiter).permit(:name, :team)
+  end
+end

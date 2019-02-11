5706c879fce3221604c1a45b416fdbbe7989aed4
Joseph-Kung <joseph.kung@hotmail.com>
2019-02-10 16:51:57 -0800

    Create recruiter routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :recruiters, only: [:create, :index, :show, :destroy]
 end
